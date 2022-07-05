6fc8297f361707a52f69cbf26479810c8ebed6f4
Vincent <vincent@thaless-MacBook-Pro.local>
2022-07-06 02:10:27 +0800

    Debug API

--- src/main/java/com/vincent/techchallenge/service/AirportService.java
+++ src/main/java/com/vincent/techchallenge/service/AirportService.java
@@ -39,6 +39,7 @@ public class AirportService {
 
     public AirportDto[] getAirports() {
         String url = this.url + "airac/airports";
+        System.out.println("Calling " + url);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<AirportDto[]> response = restTemplate.exchange(url, HttpMethod.GET, this._getHttpEntity(), AirportDto[].class);
         AirportDto[] result = response.getBody();
